ca14eda06384086137ce5d9da6219547c41783d5
sargeforce <several989@mail.ru>
2019-11-22 13:21:37 +0600

    Базовые сущности, брэнд и секции

---
+++ SargeStoreDomain/Entities/Section.cs
@@ -0,0 +1,12 @@
+﻿using SargeStoreDomain.Entities.Base;
+using SargeStoreDomain.Entities.Base.Interfaces;
+
+namespace SargeStoreDomain.Entities
+{
+    /// <summary>Секция</summary>
+    public class Section : NamedEntity, IOrderedEntity
+    {
+        public int Order { get; set; }
+        int? ParentId { get; set; }
+    }
+}
