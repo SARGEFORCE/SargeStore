d906eb5f6609a3ef28d64645553dcee7ab91260c
Sergej Salmin <several989@mail.ru>
2019-11-24 21:20:58 +0600

    Контекст БД + коннекшн стринг

---
+++ DataAccessLayer/Context/SargeStoreDB.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SargeStoreDomain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.Context
+{
+    public class SargeStoreDB : DbContext
+    {
+        public DbSet<Brand >Brands {get; set; }
+        public DbSet<Section> Sections{ get; set; }
+        public DbSet<Product> Products{ get; set; }
+
+        public SargeStoreDB(DbContextOptions<SargeStoreDB> options) : base(options) { }
+    }
+}
--- SargeStoreDomain/Entities/Base/NamedEntity.cs
+++ SargeStoreDomain/Entities/Base/NamedEntity.cs
@@ -1,10 +1,12 @@
 ﻿using SargeStoreDomain.Entities.Base.Interfaces;
+using System.ComponentModel.DataAnnotations;
 
 namespace SargeStoreDomain.Entities.Base
 {
     /// <summary>Именованная сущность</summary>
     public abstract class NamedEntity : BaseEntity, INamedEntity
     {
+        [Required]
         public string Name { get; set; }
     }
 
