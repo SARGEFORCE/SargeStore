72c36c98a93d863e8e7a1a1148a301cd6797e66b
Sergej Salmin <several989@mail.ru>
2019-11-25 21:53:03 +0600

    Ыницыализацыя базы данных  =)))

--- SargeStore/Data/SargeStoreContexInitializer.cs
+++ SargeStore/Data/SargeStoreContexInitializer.cs
@@ -1,4 +1,5 @@
 ﻿using DataAccessLayer.Context;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,9 +14,46 @@ namespace SargeStore.Data
 
         public async Task InitializeAsync()
         {
-            if(await _db.Database.EnsureDeletedAsync())
+            var db = _db.Database;
+            //if(await _db.Database.EnsureDeletedAsync())
+            //{
+            //    //БД существовала и была успешно удалена
+            //}
+
+            //await _db.Database.EnsureCreatedAsync();
+
+            await db.MigrateAsync(); //Автоматическое создание и миграция до последней версии
+
+            if (await _db.Products.AnyAsync()) return;
+
+            using (var transaction = await db.BeginTransactionAsync())
             {
-                //БД существовала и была успешно удалена
+                await _db.Sections.AddRangeAsync(TestData.Sections);
+
+                await db.ExecuteSqlCommandAsync("SET IDENTITY_INSERT [dbo].[Sections] ON");
+                await _db.SaveChangesAsync();
+                await db.ExecuteSqlCommandAsync("SET IDENTITY_INSERT [dbo].[Sections] OFF");
+                transaction.Commit();
+            }
+
+            using (var transaction = await db.BeginTransactionAsync())
+            {
+                await _db.Brands.AddRangeAsync(TestData.Brands);
+
+                await db.ExecuteSqlCommandAsync("SET IDENTITY_INSERT [dbo].[Brands] ON");
+                await _db.SaveChangesAsync();
+                await db.ExecuteSqlCommandAsync("SET IDENTITY_INSERT [dbo].[Brands] OFF");
+                transaction.Commit();
+            }
+
+            using (var transaction = await db.BeginTransactionAsync())
+            {
+                await _db.Products.AddRangeAsync(TestData.Products);
+
+                await db.ExecuteSqlCommandAsync("SET IDENTITY_INSERT [dbo].[Products] ON");
+                await _db.SaveChangesAsync();
+                await db.ExecuteSqlCommandAsync("SET IDENTITY_INSERT [dbo].[Products] OFF");
+                transaction.Commit();
             }
         }
     }
