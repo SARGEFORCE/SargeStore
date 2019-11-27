953d5e57c3ee819c680c81c8cfcd207a07fe0ddd
Sergej Salmin <several989@mail.ru>
2019-11-27 21:03:43 +0600

    Настройка контекста БД для Identity

---
+++ SargeStoreDomain/Entities/Identity/User.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SargeStoreDomain.Entities.Identity
+{
+    public class User : IdentityUser
+    {
+        public string Description { get; set; }
+    }
+
+    public class Role : IdentityRole
+    {
+        public string Description { get; set; }
+    }
+}
