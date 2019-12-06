345c9be03cda37ffe30b5a6fbd1efad164d750fb
Sergej Salmin <several989@mail.ru>
2019-12-07 00:25:09 +0600

    Add SqlOrderService и Interface

---
+++ SargeStore/Infrastructure/Interfaces/IOrderService.cs
@@ -0,0 +1,13 @@
+﻿using SargeStore.ViewModels;
+using SargeStoreDomain.Entities;
+using System.Collections.Generic;
+
+namespace SargeStore.Infrastructure.Interfaces
+{
+    public interface IOrderService
+    {
+        IEnumerable<Order> GetUserOrders(string UserName);
+        Order GetOrderById(int id);
+        Order CreateOrder(OrderViewModel OrderModel, CartViewModel CartModel, string UserName);
+    }
+}
