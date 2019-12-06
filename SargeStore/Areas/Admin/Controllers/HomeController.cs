a5c29b439d2b185319bd3c7a122e25d71e4c8689
Sergej Salmin <several989@mail.ru>
2019-12-06 23:04:58 +0600

    Представление списка товаров в области админа

--- SargeStore/Areas/Admin/Controllers/HomeController.cs
+++ SargeStore/Areas/Admin/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using SargeStore.Infrastructure.Interfaces;
 using SargeStoreDomain.Entities.Identity;
 
 namespace SargeStore.Areas.Admin.Controllers
@@ -7,6 +8,16 @@ namespace SargeStore.Areas.Admin.Controllers
     [Area("Admin"), Authorize(Roles = Role.Administrator)]
     public class HomeController : Controller
     {
+        private readonly IProductData _ProductData;
+        public HomeController(IProductData ProductData) => _ProductData = ProductData;
         public IActionResult Index() => View();
+
+        public IActionResult ProductList() => View(_ProductData.GetProducts());
+
+        public IActionResult Edit(int? id) => View();
+        public IActionResult Delete(int id) => View();
+
+        [HttpPost, ActionName(nameof(Delete))]
+        public IActionResult DeleteConfirm(int id) => RedirectToAction("ProductList");
     }
 }
