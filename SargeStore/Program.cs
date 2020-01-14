442719d31c9268e38fa8eb5134266abbb5c4b55e
Sarge <several989@mail.ru>
2020-01-14 21:58:10 +0600

    //.UseUrls("http://0.0.0.0:8080")

--- SargeStore/Program.cs
+++ SargeStore/Program.cs
@@ -21,7 +21,7 @@ namespace SargeStore
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-            .UseUrls("http://0.0.0.0:8080")
+            //.UseUrls("http://0.0.0.0:8080")
             .UseStartup<Startup>();
     }
 }
